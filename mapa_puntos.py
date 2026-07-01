2cb70b724742c40da794ebc9350b134de7b4fc78
harley huayllas <harleyjoelh@gmail.com>
2026-07-01 01:49:21 -0400

    se implemento la figura 2

--- mapa_puntos.py
+++ mapa_puntos.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 class MapaPuntos:
 
@@ -30,20 +30,55 @@ class MapaPuntos:
                     self.cota.append(float(datos[3]))
 
     def graficar(self):
-
+  
         plt.figure(figsize=(10,8))
-
         plt.scatter(
             self.este,
             self.norte,
             s=2,
             color="black"
         )
+        # Vértices del rombo 2
+        vertice_izq = (341100, 7457600)
+        vertice_sup = (346100, 7462600)
+        vertice_der = (351100, 7457600)
+        vertice_inf = (346100, 7452600)
+        
+        # Dibujar el rombo
+        x_rombo = [
+            vertice_izq[0],
+            vertice_sup[0],
+            vertice_der[0],
+            vertice_inf[0],
+            vertice_izq[0]     
+        ]
 
+        y_rombo = [
+            vertice_izq[1],
+            vertice_sup[1],
+            vertice_der[1],
+            vertice_inf[1],
+            vertice_izq[1]
+        ]
+        plt.plot(
+            x_rombo,
+            y_rombo,
+            color="red",
+            linewidth=2,
+            label="Polígono 2"
+        )
         plt.title("Mapa de Puntos")
         plt.xlabel("Coordenada Este")
         plt.ylabel("Coordenada Norte")
         plt.axis("equal")
-        plt.grid(True)
+        plt.xlim(333600, 363600)
+        plt.ylim(7442600, 7462600)
+        # Configurar la retícula cada 5000
+        xticks = np.arange(333600, 363601, 5000)
+        yticks = np.arange(7442600, 7462601, 5000)
 
+        plt.xticks(xticks, rotation=45)
+        plt.yticks(yticks)
+        plt.grid(True)
+        plt.legend()
         plt.show()
